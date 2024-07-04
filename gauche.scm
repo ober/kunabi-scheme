05719b3743b197ad656ad3fb4f570ec2fbfcc58a
ober <jaimef@linbsd.org>
2024-07-04 08:50:21 -0600

    fixup

--- gauche.scm
+++ gauche.scm
@@ -1,28 +1,26 @@
-#!/usr/bin/gosh
-;; Ensure to make this script executable or run it with Gauche by `gosh script.scm`
+#!/home/linuxbrew/.linuxbrew/bin/gosh
 
+(use rpc.zlib)        ; For gzip compression/decompression
 (use gauche.uvector)  ; For bytevector operations
 (use binary.io)       ; General I/O operations
 (use srfi-19)         ; Time and date library, only if needed
-(use rfc.json)       ; JSON parsing
+(use rfc.json)        ; JSON parsing
 (use file.util)       ; File utilities for directory walking
-(use rfc.zlib)        ; For gzip compression/decompression
 
 (define (read-json-gzip file-path)
   (call-with-input-file file-path
     (lambda (in)
       (let* ((gzip-port (zlib-inflate-port in))
              (json-string (port->string gzip-port)))
-        (json-string->object json-string)))
-    :binary #t))
+        (json-string->object json-string)))))
 
 ;; Function to check if a file has a .json.gz extension
 (define (json-gz-file? file-name)
-  (string-match ".*\\.json\\.gz$" file-name))
+  (rxmatch ".*\\.json\\.gz$" file-name))
 
 ;; Function to walk a directory tree and return list of files matching json.gz
 (define (walk-directory dir)
-  (let loop ((entries (directory-files dir))
+  (let loop ((entries (directory-list dir))
              (results '()))
     (for-each
      (lambda (entry)
@@ -30,7 +28,7 @@
          (cond
           ((and (file-exists? full-path)
                 (not (member entry '("." ".."))))
-           (if (file-directory? full-path)
+           (if (file-is-directory? full-path)
                (set! results (append results (walk-directory full-path)))
                (if (json-gz-file? entry)
                    (set! results (cons full-path results))))))))
